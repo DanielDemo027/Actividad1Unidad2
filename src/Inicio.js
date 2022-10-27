1fbce6f66504096fcb63e2310d4f85d5918614cd
DanielDemo027 <danieldotsaravia027@gmail.com>
2022-10-26 20:48:45 -0500

    Subida

---
+++ src/Inicio.js
@@ -0,0 +1,21 @@
+import FilterableProductTable from "./FilterableProductTable"
+
+const Inicio = () =>{
+    
+    const PRODUCTS = [
+        {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+        {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+        {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+        {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+        {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+        {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+      ]
+    return(
+        <div className="container">
+            <h1>Lista de productos</h1>
+            <FilterableProductTable products = {PRODUCTS}/>
+        </div>
+    )
+}
+
+export default Inicio
